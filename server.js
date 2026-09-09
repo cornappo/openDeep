a4d882ee9fe21b5c6d347acb035abff7ddc50913
cornappo <92447728+cornappo@users.noreply.github.com>
2026-09-09 17:37:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,13 +14,7 @@ app.use(express.json({ limit: '10mb' }));
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: true,
-        checkServerIdentity: (servername, cert) => {
-            if (!servername.endsWith('.supabase.com') && !servername.endsWith('.supabase.co') && !servername.endsWith('.pooler.supabase.com')) {
-                return new Error(`Certificato non valido per l'host: ${servername}`);
-            }
-            return undefined;
-        }
+        rejectUnauthorized: false
     }
 });
 
